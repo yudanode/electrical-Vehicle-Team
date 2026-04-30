8996e53b50c0a0280120b3c096ba84fd31c7ec68
Youpiiiiiiiiiiii <yudannuralif@gmail.com>
2026-04-30 16:51:49 +0700

    feat: navbar and img

--- public/portofolio/index.php
+++ public/portofolio/index.php
@@ -10,7 +10,7 @@
         tailwind.config = {
             theme: {
                 extend: {
-                                        colors: {
+                    colors: {
                         'molikom-red': '#E8001D',
                     },
                     fontFamily: {
@@ -23,15 +23,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap" rel="stylesheet" />
 </head>
 
-<body class="bg-gray-900">
+<body class="bg-gray-500">
 
-    <nav class="fixed top-0 left-0 w-full z-50 bg-black/90 backdrop-blur-sm border-b border-white/5">
-        <div class="max-w-7xl mx-auto px-6 lg:px-8">
-            <div class="flex items-center justify-between h-16">
-                
-            </div>
-        </div>
+    <nav class="flex fixed justify-betweens">
+        <a href="" class="inline-block">
+            <img src="/public/assets/img/logo/logo.png" alt="" class="h-12 w-auto block">
+        </a>
     </nav>
 
 </body>
+
 </html>

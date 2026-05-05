8e54e98bd7fd8e0e29930fd7f1e73309fa7bf434
Youpiiiiiiiiiiii <yudannuralif@gmail.com>
2026-05-05 08:37:30 +0700

    feat: add detailed description for Shembrani in portfolio section

--- public/portofolio/index.php
+++ public/portofolio/index.php
@@ -1,32 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        'molikom-red': '#E8001D',
-                    },
-                    fontFamily: {
-                        'sans': ['Barlow', 'sans-serif'],
-                    }
-                }
-            }
-        }
-    </script>
-    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap" rel="stylesheet" />
-</head>
-
 <?php $pageTitle = 'Portofolio - Molikom'; ?>
 <?php require_once '../../app/views/layouts/head.php' ?>
 <?php require_once '../../app/views/layouts/navbar.php' ?>
 
 
+<section class="bg-[#c8c8c8] py-20">
+    <div class="max-w-5x1 mx-auto px-8 ">
+        <div class="flex items-center gap-12">
+            <div class="w-[40%] shrink-0">
+                <img src="<?= BASE_URL ?>/assets/img/portofolio/shembrani.png" alt="">
+            </div>
+            <div class="flex-1 ">
+                <h2 class="text-5x1 font-extrabold text-black mb-6" style="font-family: 'Arial Black', sans-serif;">Shembrani</h2>
+                <p class="text-black text-base leading-relaxed">
+                    Sembrani Molikom lahir pada 25 Desember 2025 sebagai bentuk semangat dan kerja keras
+                    mahasiswa dalam menciptakan kendaraan listrik yang cepat, kuat, dan punya karakter.
+                    Nama Sembrani diambil dari kuda legendaris yang dikenal tangguh dan memiliki kecepatan
+                    tinggi. Nama ini dipilih karena sesuai dengan konsep unit drag EV yang harus punya tenaga
+                    besar, respons cepat, dan siap melaju di lintasan.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
-</html>
+
+
+
+<section class="bg-[#000000] py-20">
+    <div class="max-w-5x1 mx-auto px-8">
+        <div class="flex items-center gap-11">
+            <div class="shrink w-[40%] ml-8">
+                <h2 class="text-5x1 font-extrabold text-white mb-6">Fusena Evo 4</h2>
+                <p class="text-white text-base leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam illo, explicabo beatae dolorem maiores voluptatibus, cupiditate dignissimos doloremque nulla enim, laborum suscipit? Minima eos facilis in maiores id. Aspernatur consequatur, blanditiis deleniti perferendis nam totam modi, eius temporibus cum adipisci exercitationem! Voluptates repellendus placeat ea repudiandae eos rem, sequi modi molestiae omnis obcaecati quis nulla perferendis suscipit fuga, voluptatem est aut atque, alias pariatur quae nam numquam. Ex blanditiis voluptates velit possimus illum quibusdam aut culpa officia impedit nesciunt veniam, eaque esse suscipit vitae earum dolor, unde minus iste labore. Totam excepturi reprehenderit est, expedita inventore assumenda ex corrupti doloribus.</p>
+            </div>
+            <div class="flex-1">
+                <img src="<?= BASE_URL ?>/assets/img/portofolio/shembrani.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
